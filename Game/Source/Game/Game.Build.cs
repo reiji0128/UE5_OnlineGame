23f54ead9a5e52d8997e11079f065f77e219bb1e
Nagatomo <reiji128@icloud.com>
2025-04-06 04:45:32 +0900

    Steam SDK のパス設定の追加

--- Game/Source/Game/Game.Build.cs
+++ Game/Source/Game/Game.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class Game : ModuleRules
@@ -8,6 +9,20 @@ public class Game : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "OnlineSubsystem", "OnlineSubsystemSteam" });
-	}
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "OnlineSubsystemSteam","OnlineSubsystem" });
+
+        // Steamworks SDKのパスを設定
+        string SteamSDKPath = Path.Combine(ModuleDirectory, "C:/Program Files/Epic Games/UE_5.4/Engine/Source/ThirdParty/Steamworks/Steamv157/sdk");
+
+        // Steamworks SDKのヘッダーファイルへのインクルードパスを追加
+        PublicIncludePaths.Add(Path.Combine(SteamSDKPath, "public"));
+
+        // ライブラリのパスを追加
+        string SteamLibPath = Path.Combine(SteamSDKPath, "redistributable_bin/win64");
+        PublicAdditionalLibraries.Add(Path.Combine(SteamLibPath, "steam_api64.lib"));
+
+        // steam_api.dll を実行時にコピー
+        string SteamDllPath = Path.Combine(SteamLibPath, "steam_api.dll");
+        RuntimeDependencies.Add(SteamDllPath);
+    }
 }
