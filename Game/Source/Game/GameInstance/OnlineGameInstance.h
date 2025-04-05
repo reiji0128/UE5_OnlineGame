7de284da35e91d1f783ee76b9122a2441cd403aa
Nagatomo <reiji128@icloud.com>
2025-04-06 04:46:28 +0900

    OnlineGameInstance の追加
    SessionInterface の作成

---
+++ Game/Source/Game/GameInstance/OnlineGameInstance.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Interfaces\OnlineSessionInterface.h"
+#include "OnlineGameInstance.generated.h"
+/**
+ * 
+ */
+UCLASS()
+class GAME_API UOnlineGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual void Init() override;
+
+private:
+	IOnlineSessionPtr OnlineSessionInterface = nullptr;
+};
